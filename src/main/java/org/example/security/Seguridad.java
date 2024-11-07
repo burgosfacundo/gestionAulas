e00813bc3d51bbd43b8ac461a4e170eba3c4b006
burgosfacundo <facundoandresbur@gmail.com>
2024-11-07 16:13:57 -0300

    Agrego encoder a contraseña de usuarios

--- src/main/java/org/example/security/Seguridad.java
+++ src/main/java/org/example/security/Seguridad.java
@@ -5,19 +5,20 @@ import org.example.exception.AutenticacionException;
 import org.example.exception.JsonNotFoundException;
 import org.example.service.UsuarioService;
 import org.example.model.Usuario;
+import org.mindrot.jbcrypt.BCrypt;
 
 /**
  * Clase de seguridad de la app
- * Se encarga del inicio de Sesion y de validar permisos
+ * Se encarga del inicio de Sesión y de validar permisos
  */
 public class Seguridad {
     private final UsuarioService usuarioService = new UsuarioService();
 
     /**
-     * Metodo para iniciar sesion
+     * Método para iniciar sesión
      * @param username nombre de usuario
      * @param password contraseña
-     * @return Usuario que inicia sesion
+     * @return Usuario que inicia sesión
      * @throws AutenticacionException si el username o password son incorrectos
      * @throws JsonNotFoundException si no se encuentra el archivo JSON
      */
@@ -25,20 +26,20 @@ public class Seguridad {
         //Verificamos si existe un usuario con ese username y password a traves de un stream
         return usuarioService.listar().stream()
                 .filter(usuario -> usuario.getUsername().equals(username) &&
-                        usuario.getPassword().equals(password))
+                        BCrypt.checkpw(password, usuario.getPassword())) // decodifico la contraseña para validar
                 .findFirst()
                 .orElseThrow(() -> new AutenticacionException("Usuario o contraseña incorrectos"));
     }
 
 
     /**
-     * Metodo para validar permisos del usuario
-     * @param usuario usuario que quiere realizar una accion
+     * Método para validar permisos del usuario
+     * @param usuario usuario que quiere realizar una acción
      * @param permisos enum que indica que es lo que quiere hacer el usuario
      * @return boolean que indica si tiene permisos o no
      */
     public boolean verificarPermiso(Usuario usuario, Permisos permisos) {
-        //Llamamos al metodo que devuelve si el usuario tiene permiso segun su Rol
+        //Llamamos al método que devuelve si el usuario tiene permiso según su Rol
         return usuario.getRol().tienePermiso(permisos);
     }
 }
